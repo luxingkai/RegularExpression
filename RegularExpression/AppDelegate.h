b88527d666265954ed47da749697666ac9919b52
tigerfly <tigerfly@tigerflydeiMac.local>
2020-05-15 09:35:16 +0800

    log

---
+++ RegularExpression/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  RegularExpression
+//
+//  Created by tigerfly on 2020/5/15.
+//  Copyright © 2020 tigerfly. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ RegularExpressionTests/RegularExpressionTests.m
@@ -0,0 +1,37 @@
+//
+//  RegularExpressionTests.m
+//  RegularExpressionTests
+//
+//  Created by tigerfly on 2020/5/15.
+//  Copyright © 2020 tigerfly. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface RegularExpressionTests : XCTestCase
+
+@end
+
+@implementation RegularExpressionTests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
